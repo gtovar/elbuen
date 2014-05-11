5e8dd3304d4bb6f10e0ca95d9d8b693c2086af43
gtovar1985 <gtovar@incaztech.com>
2014-05-11 12:25:55 -0500

    Add DISHES controller

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,19 @@
+class DishesController < ApplicationController
+  def snack
+  end
+
+  def dessert
+  end
+
+  def elegant
+  end
+
+  def main
+  end
+
+  def entry
+  end
+
+  def salad
+  end
+end

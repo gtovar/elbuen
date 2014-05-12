fbc880c9b144e9817de0ab44b011796f86c5f7b7
gtovar1985 <gtovar@incaztech.com>
2014-05-11 19:12:44 -0500

    Add style par e impar EXITO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :history
+
+  private
+
+  def history
+    session[:history] = session[:history].to_i + 1
+  end
 end

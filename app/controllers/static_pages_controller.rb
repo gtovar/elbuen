ee053feb5bb570171ab437a2e2a5903538621288
gtovar1985 <gtovar@incaztech.com>
2014-05-30 12:44:16 -0500

    Config contact form action for send emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,12 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+    unless params.blank? 
+        c = ContactForm.new(:name => params["name"],:email => params["email"], :message => params["message"])
+        if c.deliver
+          redirect_to root_path
+        end
+    end
   end
 
   def dishes

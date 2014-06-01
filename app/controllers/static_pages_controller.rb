117f851570f40e9c011adfd6872eec01fa6b4952
gtovar1985 <gtovar@incaztech.com>
2014-06-01 18:55:57 -0500

    Send multiple sms test1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,6 +14,14 @@ class StaticPagesController < ApplicationController
           @gilberto = Twilio::REST::Client.new account_sid, auth_token
           @client.account.sms.messages.create({
               :from => '+16096442390', 
+              :to => '+5218111899317', 
+              :body => "El buen plato => #{ truncate(params["message"], length: 100)}"
+          })
+
+          sleep 2
+
+          @client.account.sms.messages.create({
+              :from => '+16096442390', 
               :to => '+5218113129230', 
               :body => "El buen plato => #{ truncate(params["message"], length: 100)}"
           })

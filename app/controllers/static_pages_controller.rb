31cf2e50d644256f953e8ff1f4fc3284bbcdab3c
gtovar1985 <gtovar@incaztech.com>
2014-06-01 21:47:49 -0500

    Fix sintaxis

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,29 +6,29 @@ class StaticPagesController < ApplicationController
 
   def contact
     unless params.blank? 
-        c = ContactForm.new(:name => params["name"],:email => params["email"], :message => params["message"])
-        if c.deliver
-          account_sid = ENV['TWILIO_ACCOUNT_SID']
-          auth_token  = ENV['TWILIO_AUTH_TOKEN']
-          client = Twilio::REST::Client.new account_sid, auth_token
+      c = ContactForm.new(:name => params["name"],:email => params["email"], :message => params["message"])
+      if c.deliver
+        account_sid = ENV['TWILIO_ACCOUNT_SID']
+        auth_token  = ENV['TWILIO_AUTH_TOKEN']
+        client = Twilio::REST::Client.new account_sid, auth_token
 
-          from = '+16096442390'
+        from = '+16096442390'
 
-          numbers = {
-            '+5218111899317' => "Uno",
-            '+5218111899317' => "Dos"
-            }
+        numbers = {
+          '+5218111899317' => "Uno",
+          '+5218111899317' => "Dos"
+        }
 
-          numbers.each do |key,value|
+        numbers.each do |key,value|
           client.account.sms.messages.create({
-              :from => '+16096442390', 
-              :to => key, 
-              :body => "El buen plato => #{ truncate(params["message"], length: 100) value }   "
+            :from => '+16096442390', 
+            :to => key, 
+            :body => "El buen plato => #{ truncate(params["message"], length: 100) }, #{value} "
           })
-          end
-
-          redirect_to root_path
         end
+
+        redirect_to root_path
+      end
     end
   end
 

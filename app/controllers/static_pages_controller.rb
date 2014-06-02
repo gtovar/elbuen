42de74df84f56f4725c68668203414f5ae5c4f75
gtovar1985 <gtovar@incaztech.com>
2014-06-01 21:37:33 -0500

    Update test number2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,21 +10,22 @@ class StaticPagesController < ApplicationController
         if c.deliver
           account_sid = ENV['TWILIO_ACCOUNT_SID']
           auth_token  = ENV['TWILIO_AUTH_TOKEN']
-          @client = Twilio::REST::Client.new account_sid, auth_token
-          @gilberto = Twilio::REST::Client.new account_sid, auth_token
-          @client.account.sms.messages.create({
-              :from => '+16096442390', 
-              :to => '+5218111899317', 
-              :body => "El buen plato => #{ truncate(params["message"], length: 100)}"
-          })
+          client = Twilio::REST::Client.new account_sid, auth_token
 
-          sleep 2
+          from = '+16096442390'
 
-          @client.account.sms.messages.create({
+          numbers = {
+            '+5218111899317' => "Uno",
+            '+5218111899317' => "Dos"
+            }
+
+          numbers.each do |key,value|
+          client.account.sms.messages.create({
               :from => '+16096442390', 
-              :to => '+5218113129230', 
-              :body => "El buen plato => #{ truncate(params["message"], length: 100)}"
+              :to => key, 
+              :body => "El buen plato => #{ truncate(params["message"], length: 100) value }   "
           })
+          end
 
           redirect_to root_path
         end
